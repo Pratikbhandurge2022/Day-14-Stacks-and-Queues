d9a624c58473bf67a8822fafaa32d2f346143fe0
Pratikbhandurge2022 <pratikbhandurge2222@gmail.com>
2022-07-06 00:18:35 +0530

    UC4

--- Datastructure/Datastructure/Program.cs
+++ Datastructure/Datastructure/Program.cs
@@ -6,12 +6,20 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            
+           
             Queue queue = new Queue(3);
             queue.Enqueue(56);
             queue.Enqueue(30);
             queue.Enqueue(70);
-            Console.WriteLine(queue.ToString());
+
+            Console.WriteLine(queue.ToString());                                   
+            Console.WriteLine("Dequeue method removed " + queue.Dequeue());       
+            Console.WriteLine(queue.ToString());                                   
+            Console.WriteLine("Dequeue method removed " + queue.Dequeue());        
+            Console.WriteLine(queue.ToString());                                   
+            Console.WriteLine("Dequeue method removed " + queue.Dequeue());       
+            Console.WriteLine(queue.ToString());                                   
+
         }
     }
 }
