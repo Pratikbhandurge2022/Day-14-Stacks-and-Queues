3367b295c40e1f698cb97fc21b58ed659999e886
Pratikbhandurge2022 <pratikbhandurge2222@gmail.com>
2022-07-06 00:10:14 +0530

    initial commit

---
+++ Datastructure/Datastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Datastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
